495f481e5894e5067cdd6deb9ce0d6cd0ef93e50
Twinkle Sharma <smtwinkle451@gmail.com>
2021-02-28 02:50:00 +0530

    Using Multi tags/JSX Elements

--- tut-app/src/index.js
+++ tut-app/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Also we Can use require instead of import
-// var React = require('react');
-// var ReactDOM = require('react-dom');
-
-
 //Syntax in Hindi is render.( Kya Dikhana hai , Kahana Dikhana hai )
 ReactDOM.render(
-  <h1>Hi There, I am Twinkle Sharma</h1>, document.getElementById('root')
+  <div>
+  <h1>Hi There, I am Twinkle Sharma</h1>
+  <h2>I am a Sophomore at Computer Science & Engineering</h2>
+  <h2>I want to be a Software Developer Engineer in Future</h2>
+  <h2>My Skills are:-</h2>
+  <ul>
+    <li>Web Development</li>
+    <li>Core Java & C++ </li>
+    <li>OOPs & DBMS</li>
+    <li>Git & Graphics Designing</li>
+  </ul>
+  </div>, document.getElementById('root')
 );
