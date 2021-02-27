7171e8410c720aee6580345d2a0c8ef7955a9f92
Twinkle Sharma <smtwinkle451@gmail.com>
2021-02-28 03:45:52 +0530

    Inline CSS in JSX Element

--- tut-app/src/index.js
+++ tut-app/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Don't Forget 
+// 1. Convert kebab-case to camelCase
+// 2. Pass Object inside style={}
+// 3. {} means here is something javascript expression
 ReactDOM.render(
   <>
-  <h1>Hi There, I am Twinkle Sharma</h1>
-  <h2>I am a Sophomore at Computer Science & Engineering</h2>
+  <h1 style = {{color:'#d23e4d',textTransform:'capitalize'}}>Hi There, I am Twinkle Sharma</h1>
+  <h2 style={{fontFamily:'"Lucida Console", "Courier New", "monospace"' ,textDecoration:'underline',color:'rgb(5, 93, 255)'}}>I am a Sophomore at Computer Science & Engineering</h2>
+  <div style={{border:'5px solid black', padding:'3px 10px'}}>
   <h2>I want to be a Software Developer Engineer in Future</h2>
   <h2>My Skills are:-</h2>
   <ul>
@@ -13,5 +18,6 @@ ReactDOM.render(
     <li>OOPs & DBMS</li>
     <li>Git & Graphics Designing</li>
   </ul>
+  </div>
   </>, document.getElementById('root')
 );
