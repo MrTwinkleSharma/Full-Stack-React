b2b87eea13334fbf5cebcd98c56209d3158cb43b
Twinkle Sharma <smtwinkle451@gmail.com>
2021-02-28 04:03:51 +0530

    External CSS in React

--- tut-app/src/index.js
+++ tut-app/src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Don't Forget 
-// 1. Convert kebab-case to camelCase
-// 2. Pass Object inside style={}
-// 3. {} means here is something javascript expression
+//import css file with a relative path & everything remains same
+import './index.css'; 
 
-const style1 = {color:'#d23e4d',textTransform:'capitalize'};
-const style2 = {fontFamily:'"Lucida Console", "Courier New", "monospace"' ,textDecoration:'underline',color:'rgb(5, 93, 255)'};
-const style3 = {border:'5px solid black', padding:'3px 10px'};
 ReactDOM.render(
   <>
-  <h1 style = {style1}>Hi There, I am Twinkle Sharma</h1>
-  <h2 style = {style2}>I am a Sophomore at Computer Science & Engineering</h2>
-  <div style = {style3}>
+  <h1 id = 'header'>Hi There, I am Twinkle Sharma</h1>
+  <h2 id = 'about'>I am a Sophomore at Computer Science & Engineering</h2>
+  <div id = 'skills'>
   <h2>I want to be a Software Developer Engineer in Future</h2>
   <h2>My Skills are:-</h2>
   <ul>
