5e80fa3af5fca87c593370554001f0a0caf734a3
Twinkle Sharma <smtwinkle451@gmail.com>
2021-02-28 04:30:51 +0530

    CSS with JS Expression

--- tut-app/src/index.js
+++ tut-app/src/index.js
@@ -4,10 +4,36 @@ import ReactDOM from 'react-dom';
 //import css file with a relative path & everything remains same
 import './index.css'; 
 
+var greet,greetcolor,backgroundColor;
+var d = new Date();
+var n = d.getHours();
+
+if(n<12)
+{
+  greet = 'Morning';
+  greetcolor = 'yellow';
+  backgroundColor = '#fc5947';
+}
+else if(n>=12 && n<17)
+{
+  greet = 'Afternoon';
+  greetcolor = 'red';
+  backgroundColor = '#406cff';
+  
+}
+else 
+{
+  greet = 'Evening';
+  greetcolor = 'green';
+  backgroundColor = '#fc477a';
+
+}
+
+
 ReactDOM.render(
   <>
-  <h1 id = 'header'>Hi There, I am Twinkle Sharma</h1>
-  <h2 id = 'about'>I am a Sophomore at Computer Science & Engineering</h2>
+  <h1 id = 'header' style={{color:greetcolor, backgroundColor:backgroundColor}}>Good {greet}</h1>
+  <h2 id = 'about'> I am Twinkle Sharma, Sophomore at Computer Science & Engineering</h2>
   <div id = 'skills'>
   <h2>I want to be a Software Developer Engineer in Future</h2>
   <h2>My Skills are:-</h2>
