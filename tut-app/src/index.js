4ba6b4ce29a0217b2d2b8659bff2992053b8a706
Twinkle Sharma <smtwinkle451@gmail.com>
2021-02-28 01:51:20 +0530

    Hi Message Printed

--- tut-app/src/index.js
+++ tut-app/src/index.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Also we Can use require instead of import
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+
+//Syntax in Hindi is render.( Kya Dikhana hai , Kahana Dikhana hai )
+ReactDOM.render(
+  <h1>Hi There, I am Twinkle Sharma</h1>, document.getElementById('root')
+);
