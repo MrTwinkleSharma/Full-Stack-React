93bf768670421dda26f8659dc370334b5abfa89e
Twinkle Sharma <smtwinkle451@gmail.com>
2021-02-28 03:55:23 +0530

    Internal CSS in JSX Element

--- tut-app/src/index.js
+++ tut-app/src/index.js
@@ -5,11 +5,15 @@ import ReactDOM from 'react-dom';
 // 1. Convert kebab-case to camelCase
 // 2. Pass Object inside style={}
 // 3. {} means here is something javascript expression
+
+const style1 = {color:'#d23e4d',textTransform:'capitalize'};
+const style2 = {fontFamily:'"Lucida Console", "Courier New", "monospace"' ,textDecoration:'underline',color:'rgb(5, 93, 255)'};
+const style3 = {border:'5px solid black', padding:'3px 10px'};
 ReactDOM.render(
   <>
-  <h1 style = {{color:'#d23e4d',textTransform:'capitalize'}}>Hi There, I am Twinkle Sharma</h1>
-  <h2 style={{fontFamily:'"Lucida Console", "Courier New", "monospace"' ,textDecoration:'underline',color:'rgb(5, 93, 255)'}}>I am a Sophomore at Computer Science & Engineering</h2>
-  <div style={{border:'5px solid black', padding:'3px 10px'}}>
+  <h1 style = {style1}>Hi There, I am Twinkle Sharma</h1>
+  <h2 style = {style2}>I am a Sophomore at Computer Science & Engineering</h2>
+  <div style = {style3}>
   <h2>I want to be a Software Developer Engineer in Future</h2>
   <h2>My Skills are:-</h2>
   <ul>
