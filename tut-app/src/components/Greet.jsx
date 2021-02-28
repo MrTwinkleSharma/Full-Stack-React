c1b432528f60f8c2e031dc017c1dca356cff95f6
Twinkle Sharma <smtwinkle451@gmail.com>
2021-03-01 03:23:19 +0530

    props.children

--- tut-app/src/components/Greet.jsx
+++ tut-app/src/components/Greet.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const Greet = props => <h1>Hello {props.name}!</h1>;
+// const Greet = props => <h1>Hello {props.name}!</h1>;
 
-// function Greet(props)
-// {
-//     return <h1>Hello {props.name}</h1>;
-// }
+function Greet(props)
+{
+    // console.log(props);
+    return  (
+    <>
+    <h1>Hello {props.name}</h1>
+    <h2>{props.children}</h2>
+    </>     );
+}
 
 export default Greet;
--- tut-app/src/index.js
+++ tut-app/src/index.js
@@ -7,7 +7,7 @@ import  Greet from "./components/Greet";
 
 ReactDOM.render( <> <Greet name="Mr. Tiwari"/>
   <h2>and</h2>
-  <Greet name="Mr. Tripathi"/>
+  <Greet name="Mr. Tripathi">This is a Child of component</Greet>
   <Myname/>  
   </>
   ,document.getElementById('root')
