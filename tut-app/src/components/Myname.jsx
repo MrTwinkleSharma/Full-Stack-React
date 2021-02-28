98deb3ed835862225109780a00c4dbfd04643378
Twinkle Sharma <smtwinkle451@gmail.com>
2021-03-01 01:36:25 +0530

    Function Component

---
+++ tut-app/src/components/Myname.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Myname(){
+    return (<h1>My name is Twinkle Sharma !</h1>);
+}
+
+export default Myname;
--- tut-app/src/index.js
+++ tut-app/src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <>
-  </>
-  
+// Import component here
+import Myname from "./components/Myname";
+
+//Now <Myname> is our JSX element and we can use it anywhere.
+ReactDOM.render( <Myname/>  
   ,document.getElementById('root')
 );
+
+
+
+/*
+We call ReactDOM.render() with the <Myname> element.
+React calls the Myname component
+Our Myname component returns a <h1>My name is Twinkle Sharma !</h1> element as the result.
+React DOM efficiently updates the DOM to match <h1>My name is Twinkle Sharma !</h1>.
+*/
