1785a47f1cf4571ab346fb0b490bdc8d102d9595
Twinkle Sharma <smtwinkle451@gmail.com>
2021-03-02 02:56:41 +0530

    State in React

--- tut-app/src/components/Classcomponent.jsx
+++ tut-app/src/components/Classcomponent.jsx
@@ -3,9 +3,31 @@
 import React, { Component } from 'react';
 
 class Classcomponent extends Component{
+    constructor(){
+        super();
+        this.state = {
+            myname:"Mr. Twinkle Sharma",
+            iam : "Sophomore at Computer Science Engineering",
+            message: "Do checkout my Website"
+        }
+    }
+    changeState(){
+    this.setState({message : "Thanks for visitng my Website"});
+    document.getElementById('button').hidden = true;
+    
+    }
     render()
     {
-        return <h1>Hello {this.props.userName}, Welcome to our Webpage!</h1>
+        return (<>
+        <h1>Hello there, Welcome to our Webpage!</h1>
+        <h2>My name is {this.state.myname}</h2>
+        <h2>I am {this.state.iam}</h2>
+        <h2>{this.state.message}</h2>
+        <a id="button" target="_blank" href="https://mrtwinklesharma.github.io/MrTwinkleSharma/Temp-Portfolio2/">
+        <button onClick={()=>this.changeState()} >Website</button>
+        </a>
+        
+        </>)
     }
 }
 
