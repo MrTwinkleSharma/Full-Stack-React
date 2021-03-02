f1bec0edd0399d77373bebfc0257890b5696267e
Twinkle Sharma <smtwinkle451@gmail.com>
2021-03-03 01:31:33 +0530

    Event Handling Initial

--- tut-app/src/components/Myname.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const Myname = () =><h1>My name is Twinkle Sharma !</h1>;
-export {Myname}; //or use export just before const keyword
--- tut-app/src/index.js
+++ tut-app/src/index.js
@@ -1,9 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Classcomponent from './components/Classcomponent.jsx';
-
-ReactDOM.render( <Classcomponent userName="Shubhankar"/>
-  ,document.getElementById('root')
-);
-
+ReactDOM.render(<App/>, document.getElementById('root'));
