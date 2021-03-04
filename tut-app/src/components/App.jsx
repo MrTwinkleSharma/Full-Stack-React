401a47322fee15cdd68e2840b1b8fa8b800b8d98
Twinkle Sharma <smtwinkle451@gmail.com>
2021-03-04 20:37:29 +0530

    FC Event Handling

--- tut-app/src/components/App.jsx
+++ tut-app/src/components/App.jsx
@@ -1,1 +1,9 @@
 import React from 'react';
+import ConsoleClick from './Functioncomponent';
+
+function App() {
+    return <ConsoleClick/>
+}
+
+export default App;
+
--- tut-app/src/components/Functioncomponent.jsx
+++ tut-app/src/components/Functioncomponent.jsx
@@ -1,1 +1,22 @@
 import React from 'react';
+
+let i = 0;
+function clickHandler(){
+    let str = `Button is Clicked ${i++} times.`;
+    console.log(str);
+    
+}
+
+function ConsoleClick(){
+    return (
+        <>
+        <h1>Hey there! I am a Functional Component</h1>
+        <h2>My Task is to Print the message on console for Every Click on Button </h2>
+        <button onClick =  {clickHandler}> Click Me</button>
+        </>
+
+    )
+}
+
+
+export default ConsoleClick ;
