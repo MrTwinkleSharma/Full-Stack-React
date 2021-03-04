4032c2d6f7a161c485718e9e8a8e63bc8d8e7409
Twinkle Sharma <smtwinkle451@gmail.com>
2021-03-04 22:10:35 +0530

    Binding Event Handlers

--- tut-app/src/components/Classcomponent.jsx
+++ tut-app/src/components/Classcomponent.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
-class consoleClick extends Component{
-    
+class ConsoleClick extends Component{
+    constructor(){
+        super();
+        this.state = {
+            counter : 0
+        }
+    }
     clickHandler(){
-        console.log("Button Clicked");
+        console.log("Button Clicked " , this.state.counter , " Times.");
+        let i = this.state.counter;
+        i++;
+        this.setState( {counter : i} );
         
     }
     render()
@@ -13,10 +21,11 @@ class consoleClick extends Component{
         <h1>Hey there! I am a Class Component</h1>
 
         <h2>My Task is to Print the message on console for Every Click on Button </h2>
-        <button onClick =  {this.clickHandler}> Click Me</button>
+
+        <button onClick =  {this.clickHandler.bind(this)}> Click Me</button>
         </>
         )
     }
 }
 
-export default consoleClick;
+export default ConsoleClick;
