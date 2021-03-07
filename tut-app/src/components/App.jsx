81d84f6cab8bdc3e83b44babc66fe69ee89f7c95
Twinkle Sharma <smtwinkle451@gmail.com>
2021-03-08 00:08:38 +0530

    ONLY CLASS

--- tut-app/src/components/App.jsx
+++ tut-app/src/components/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Functioncomponent from './Functioncomponent';
+import React, { Component } from 'react';
+import Classcomponent from './Classcomponent'
 
-
-function App() {
-    return <Functioncomponent/>
+class App extends Component {
+    render(){
+    return <Classcomponent/>
+    }
 }
-
 export default App;
 
---
+++ tut-app/src/components/Classcomponent.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Classcomponent extends Component{
+    render(){
+        
+        return <h1>Hi, I am a Class Component</h1>
+    }
+}
+
+export default Classcomponent;
