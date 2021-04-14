fc66f07a335fd67a01075b3bd6e4a6b6cad644bf
Twinkle Sharma <smtwinkle451@gmail.com>
2021-04-15 04:04:24 +0530

    HTTP Post

--- tut-app/src/components/APICallDemo.jsx
+++ tut-app/src/components/APICallDemo.jsx
@@ -5,41 +5,48 @@ class APICallDemo extends Component {
     constructor(props){
         super(props);
         this.state = {
-            jsonlist:[],
-            error:null
+            userid:'',
+            data:'',
+            body:''
         }
     }
-    componentDidMount()
-    {
-        axios.get('https://jsonplaceholder.typicode1.com/posts')
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+    submitHandler = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+        axios.post('https://jsonplaceholder.typicode.com/posts', this.state)
         .then(response => {
-            this.setState({
-                jsonlist:response.data
-            })
+            console.log(response);
         })
         .catch(error=> {
             console.log(error);
-            this.setState({
-                error:"Error Retrieving Data :("
-            })
         })
     }
+    
 
     render(){
-        const {jsonlist,error} = this.state
+        
     return <>
-    <h1>
-        List of Fetched Data
-    </h1>
-    <div>{
-    jsonlist.length ?
-        jsonlist.map( data => <div key={data.id} > {data.body}</div>  ): null
-    }
-    {
-        error ? this.state.error :null  
-    }
-    </div>
-
+    <h1>Enter Data to Submit through POST</h1>
+    <form onSubmit={this.submitHandler}>
+        <div>
+            User Id: 
+            <input type="text" name='userid' value={this.state.userid} onChange={this.changeHandler}/>
+        </div>
+        <div>
+            Data:
+            <input type="text" name='data' value={this.state.data} onChange={this.changeHandler}/>
+        </div>
+        <div>
+            Body:
+            <input type="text" name='body'datavalue={this.state.body} onChange={this.changeHandler}/>
+        </div>
+    <button type="submit">Submit</button>
+    </form>
     </>
     }
 
