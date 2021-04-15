11857f66134eda88f609f484993c9c804e71ecae
Twinkle Sharma <smtwinkle451@gmail.com>
2021-04-15 06:56:57 +0530

    Hook Counter based on PreviousState

--- tut-app/src/components/HookCounter.jsx
+++ tut-app/src/components/HookCounter.jsx
@@ -2,9 +2,31 @@ import React, { useState } from 'react';
 
 
 function HookCounter () {
-    const [count, setCount] = useState(0);    
+    const initialCount = 0;
+    const [count, setCount] = useState(initialCount);    
+    
+   function incrementTwo(){
+        // setCount(count+2); //This will Work Fine
+
+        //This will Not
+        // for(let i = 0;i<2;i++){
+        //     setCount(count+1);
+        // }
+
+        for(let i = 0;i<2;i++){
+            setCount(prevCount => prevCount+1);
+        }
+    }
+
+
     return <>
-    <button onClick = {()=>setCount(count+1)}>Clicked {count} Times!</button>
+    <div>Clicked {count} Times!</div>
+    <span>
+        <button onClick = {()=>setCount(count-1)}>Decrement One</button>
+        <button onClick = {incrementTwo}>Increment Two </button>
+        <button onClick = {()=>setCount(count+1)}>Increment One</button>
+        <button onClick = {()=>setCount(initialCount)}>Reset Counter</button>
+    </span>
     </>
 }
 
