d780786bfe77e352922c1442bd327dcff7329538
Twinkle Sharma <smtwinkle451@gmail.com>
2021-04-18 02:22:30 +0530

    useContext

--- tut-app/src/components/ComponentC.jsx
+++ tut-app/src/components/ComponentC.jsx
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {UserContext1,UserContext2} from './App';
 
 
 function ComponentC (){
-
+        const myName =  useContext(UserContext1);
+        const iAm = useContext(UserContext2);
+        
         return <>
         This is the 3rd Level Nested Component !
-        <UserContext1.Consumer>      
-                {
-                        myName =>{                                
-                                return  <UserContext2.Consumer>
-                                        {                                        
-                                                iAm => {
-                                                        return <h3>I am {myName}, a {iAm}.</h3>
-                                                }
-                                        }
-                                 </UserContext2.Consumer>
-                        }
-                }
-        </UserContext1.Consumer>
+        <h3>I am {myName}, a {iAm}.</h3>
+                                                
         <h6>Note: "Mr. Twinkle Sharma" Coming from UserContext1 and "Full Stack Developer" from its Child UserContext2. </h6>
         </>
 }
