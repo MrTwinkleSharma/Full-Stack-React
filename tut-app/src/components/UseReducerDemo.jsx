f1bdc2515f0b2fd0979181de1bb8b1c3b9a7549b
Twinkle Sharma <smtwinkle451@gmail.com>
2021-04-18 09:28:35 +0530

    useReducer Complex Action & State

--- tut-app/src/components/UseReducerDemo.jsx
+++ tut-app/src/components/UseReducerDemo.jsx
@@ -1,7 +1,9 @@
 import React, { useReducer } from 'react';
 
 const initialState = {
-        counter:0
+        counter:0,
+        counterClone:0
+
 };
 
 const reducer = (currentState, action) => {
@@ -11,16 +13,23 @@ const reducer = (currentState, action) => {
         {
                 case 'Increment':
                         // console.log(currentState);
-                        return {counter : currentState.counter + action.value}
+                        return {...currentState, counter : currentState.counter + action.value}
                         // console.log(currentState);
                         // return currentState;
                 case 'Decrement':
                         // console.log(currentState);
-                        return {counter : currentState.counter - action.value}
+                        return {...currentState, counter : currentState.counter - action.value}
                         // console.log(currentState);
                         // return currentState;
                 case 'Reset':
-                        return initialState;
+                        return {...currentState, counter: initialState.counter};
+
+                case 'IncrementClone':
+                        return {...currentState, counterClone : currentState.counterClone + action.value}
+                case 'DecrementClone':
+                        return {...currentState,counterClone : currentState.counterClone - action.value}
+                case 'ResetClone':
+                        return {...currentState, counterClone:initialState.counterClone};
                 default:
                         return currentState;
         }
@@ -39,13 +48,16 @@ function UseReducerDemo(){
         <h3>Current Counter is {currentState.counter}</h3>
         <button onClick={()=>{dispatch({type:'Increment',value:1})}}>Increment</button>
         <button onClick={()=>{dispatch({type:'Decrement',value:1})}}>Decrement</button>
-
         <button onClick={()=>{dispatch({type:'Reset'})}}>Reset</button>
 
-        <div>
-        <button onClick={()=>{dispatch({type:'Increment',value:5})}}>Increment 5</button>
-        <button onClick={()=>{dispatch({type:'Decrement',value:5})}}>Decrement 5</button>
-        </div>
+        <hr/>
+
+        <h3>Current Counter is {currentState.counterClone}</h3>
+        <button onClick={()=>{dispatch({type:'IncrementClone',value:1})}}>IncrementClone</button>
+        <button onClick={()=>{dispatch({type:'DecrementClone',value:1})}}>DecrementClone</button>
+        <button onClick={()=>{dispatch({type:'ResetClone'})}}>ResetClone</button>
+
+        
         </>
 }
 
