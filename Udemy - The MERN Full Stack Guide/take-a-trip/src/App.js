11a4e9b4bd061cc7151ef6386cb19605fed17be5
Twinkle Sharma <smtwinkle451@gmail.com>
2021-04-22 17:49:02 +0530

    Router Demo

--- Udemy - The MERN Full Stack Guide/take-a-trip/src/App.js
+++ Udemy - The MERN Full Stack Guide/take-a-trip/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './index.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App (){
 
-  return <h1>Let's Start</h1>
+  return <Router>
+  <Route path="/" exact>
+    <h1>Home Page </h1>
+    <div>This will get rendered on <b>localhost:3000/ </b></div>
+  </Route>
+  <Route path="/users" exact>
+    <h1>Users Page </h1>
+    <div>This will get rendered on <b>localhost:3000/users </b></div>
+  </Route>
+  </Router>
 }
 export default App;
