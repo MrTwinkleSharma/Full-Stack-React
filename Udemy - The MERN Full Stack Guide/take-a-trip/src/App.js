cbc9450becfb6440ed6ba639c93c9b33300eef77
Twinkle Sharma <smtwinkle451@gmail.com>
2021-04-22 17:52:18 +0530

    Router Demo II

--- Udemy - The MERN Full Stack Guide/take-a-trip/src/App.js
+++ Udemy - The MERN Full Stack Guide/take-a-trip/src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './index.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route,Redirect,Switch} from 'react-router-dom';
 
 function App (){
 
   return <Router>
-  <Route path="/" exact>
-    <h1>Home Page </h1>
-    <div>This will get rendered on <b>localhost:3000/ </b></div>
-  </Route>
-  <Route path="/users" exact>
-    <h1>Users Page </h1>
-    <div>This will get rendered on <b>localhost:3000/users </b></div>
-  </Route>
+  <Switch>
+    <Route path="/" exact>
+      <h1>Home Page </h1>
+      <div>This will get rendered on <b>localhost:3000/ </b> and <b>Default Redirecting</b> </div>
+    </Route>
+    <Route path="/users" exact>
+      <h1>Users Page </h1>
+      <div>This will get rendered on <b>localhost:3000/users </b></div>
+    </Route>
+    <Route path="/login" exact>
+      <h1>Login Page </h1>
+      <div>This will get rendered on <b>localhost:3000/login </b></div>
+    </Route>    
+    <Redirect to='/' />
+  </Switch>
   </Router>
 }
 export default App;
