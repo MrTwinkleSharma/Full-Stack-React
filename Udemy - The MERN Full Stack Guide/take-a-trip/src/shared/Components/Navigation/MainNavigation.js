aed206b6d125a965971576e8834c12df25337045
Twinkle Sharma <smtwinkle451@gmail.com>
2021-04-24 10:12:57 +0530

    Nav Links

--- Udemy - The MERN Full Stack Guide/take-a-trip/src/shared/Components/Navigation/MainNavigation.js
+++ Udemy - The MERN Full Stack Guide/take-a-trip/src/shared/Components/Navigation/MainNavigation.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React from 'react'
 import { Link } from 'react-router-dom';
+
 import MainHeader from './MainHeader';
+import NavLinks from './NavLinks';
 
 import './MainNavigation.css'
 import './MainHeader.css';
@@ -17,7 +19,7 @@ function MainNavigation (props){
             <Link to='/'> Your Places </Link>
         </h1>
         <nav>
-
+            <NavLinks/>
         </nav>
     </MainHeader>
 }
---
+++ Udemy - The MERN Full Stack Guide/take-a-trip/src/shared/Components/Navigation/NavLinks.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './NavLinks.css';
+
+function Navlinks(props)
+{
+
+    return <ul className='nav-links'>
+    <li>
+    <NavLink to='/users' exact> ALL USERS</NavLink>
+    </li>
+    <li>
+        <NavLink to='/id/places' >MY PLACES</NavLink>
+    </li> 
+    <li>
+        <NavLink to='/newplace' >ADD PLACE</NavLink>
+    </li> 
+    <li>
+        <NavLink to='/auth' >AUTHENTICATE</NavLink>
+    </li> 
+    </ul>
+}
+
+export default Navlinks;
