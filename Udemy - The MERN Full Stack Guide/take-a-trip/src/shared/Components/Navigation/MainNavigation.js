a61f4220b5873d27cb4dd6641a89370abbf2912a
Twinkle Sharma <smtwinkle451@gmail.com>
2021-04-25 18:23:25 +0530

    Backdrop Created

--- Udemy - The MERN Full Stack Guide/take-a-trip/src/shared/Components/Navigation/MainNavigation.js
+++ Udemy - The MERN Full Stack Guide/take-a-trip/src/shared/Components/Navigation/MainNavigation.js
@@ -1,22 +1,35 @@
 import React from 'react'
+import {useState } from 'react'
 import { Link } from 'react-router-dom';
+
 
 import MainHeader from './MainHeader.js';
 import SideDrawer from './SideDrawer.js';
 import Navlinks from './NavLinks.js';
-
+import BackDrop from '../UIElements/BackDrop.js'
 import './MainNavigation.css'
 
 function MainNavigation (props){
 
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+    
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    }
+    
     return <>
+    {
+        drawerIsOpen && <BackDrop onClick={closeDrawer}/>
+    }
+    { drawerIsOpen && 
     <SideDrawer>
         <nav className='main-navigation__drawer-nav'>
             <Navlinks/>
         </nav>
     </SideDrawer>
+    }
     <MainHeader>
-        <button className='main-navigation__menu-btn'>
+        <button className='main-navigation__menu-btn' onClick={()=>{setDrawerIsOpen(true)}}>
             <span/>
             <span/>
             <span/>
