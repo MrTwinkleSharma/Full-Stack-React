a67631e66450241d602b4bd065e1e886d027a25b
Twinkle Sharma <smtwinkle451@gmail.com>
2021-04-27 06:51:35 +0530

    Input Element useReducer

--- Udemy - The MERN Full Stack Guide/take-a-trip/src/places/Pages/NewPlace.js
+++ Udemy - The MERN Full Stack Guide/take-a-trip/src/places/Pages/NewPlace.js
@@ -7,7 +7,7 @@ function NewPlace (props)
 
     return <>
     <form className='place-form'>
-        <Input element = 'input' id={`1`} type='text' label='Title'/>
+        <Input element = 'input' id={`1`} type='text' label='Title' validation={[]} errorText='Please Enter a Valid Title'/>
     </form>
     </>
 }
--- Udemy - The MERN Full Stack Guide/take-a-trip/src/shared/Components/FormElements/Input/Input.js
+++ Udemy - The MERN Full Stack Guide/take-a-trip/src/shared/Components/FormElements/Input/Input.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, {useReducer} from 'react';
+
 
 import './Input.css';
 
+
+const initialValue = {value:'', isValid:false};
+
+const reducer = (currentValue, action) =>{
+  switch(action.type)
+  {
+    case 'change':
+      return {
+        ...currentValue, 
+        value:action.val,
+        isValid:true
+      };
+
+      default:
+        return currentValue;
+
+  }
+}
 const Input = props => {
   
+  const [currentValue, dispatch] = useReducer(reducer, initialValue);
+
+  const changeHandler = event => {
+    dispatch({type:'change', val:event.target.value})
+}
+
   const element = props.element==='input' ?(
 
-    <input id={props.id} type={props.type} placeholder={props.placeholder} />
+    <input id={props.id} type={props.type} placeholder={props.placeholder} value={currentValue.value} onChange={changeHandler} />
 
     ) : (
 
-      <textarea id={props.id} rows={props.rows || 3}/>
+      <textarea id={props.id} rows={props.rows || 3} value={currentValue.value} onChange={changeHandler}/>
 
     );
 
-  return <div className={`form-control`}>
+  return <div className={`form-control ${!currentValue.isValid && 'form-control--invalid'}`}>
    <label htmlFor={props.id} > {props.label}</label>
    {element}
+   {!currentValue.isValid && <p>{props.errorText}</p>}
   </div>
 };
 
