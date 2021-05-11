ecfc6b611625aa52cb5c69ff527af04fd9082dc7
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-11 17:02:00 +0530

    sending response from server

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer(function(req, res){
+    res.end("Server Works....")
+});
 
 server.listen(3000,'localhost', function(){
     console.log("Server is Listening at Port 3000!");
