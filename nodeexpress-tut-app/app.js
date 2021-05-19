ae122c432440585b9fe37e790ad31054e46173d4
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-19 21:17:57 +0530

    Request/Response in http

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -5,7 +5,22 @@ const server = http.createServer(function(req, res){
     console.log(req.url, req.method);
     
     res.setHeader('content-type', 'text/html');
-    res.end("<h2>Server Works </h2>");
+    
+    if(req.method == "POST"){
+        let body = '';
+        req.on('end',()=>{
+            console.log(body);
+            res.end("<h2>Got the POST request</h2>")
+        });
+
+        req.on('data', (chunks)=>{
+            body+=chunks;
+        });
+    }
+
+    else 
+    res.end('<form method="POST"> <input type="text" name="username"><button type="submit">Create User</button></form>');
+
 });
 
 
