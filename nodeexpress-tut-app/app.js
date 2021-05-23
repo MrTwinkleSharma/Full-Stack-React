2a569f1285ba58823f7b327335f413856cfa651f
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-24 02:08:49 +0530

    Rendering html file from Server

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -1,32 +1,28 @@
 const http = require('http');
+const {readFileSync}  = require('fs');
+
+const indexhtml = readFileSync('index.html');
 
 const server = http.createServer(function(req, res){
     console.log("Request is Incoming");
-    console.log(req.url, req.method);
-    res.setHeader('Content-type', 'text/html')
-        
+    console.log(req.url, req.method);  
+    
     if(req.url=== '/')
-    {
-        
-        res.write("<h2>Welcome to Home Page</h2>")
+    { 
+        res.writeHead(200,{'content-type': 'text/html'});
+        res.write(indexhtml)
         res.end();
     }
-    if(req.url==='/about')
-    {
-
-        res.write(`<h2>This is About Page</h2>
-                <p>Hi, My name is Twinkle Sharma, 
-                I am Sophomore at Computer Science & Engineering</p>`);
-        res.end();
+    else{
+        res.writeHead(404,{'content-type': 'text/html'});
+        res.end(`
+        <h2>Oops!</h2>
+        <p>The page you were Looking for doesn't exist</p>
+        <a href='/'>Go to HomePage</a>
+        `);
     }
-
-    res.end(`
-    <h2>Oops!</h2>
-    <p>The page you were Looking for doesn't exist</p>
-    <a href='/'>Go to HomePage</a>
-    `);
 });
--
+
 server.listen(3000,'localhost', function(){
     console.log("Server is Listening at Port 3000!");
 });
