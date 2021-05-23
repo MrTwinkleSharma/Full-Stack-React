19e8807f74a62da04a4607a7e9a16df7a152db36
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-24 03:17:49 +0530

    Basic response sending with Routing

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -7,6 +7,14 @@ app.get('/', (req, res)=>{
     res.send("Home Page")
 })
 
+app.get('/about', (req, res)=>{
+    res.send("About Page")
+})
+
+app.all('*', (req, res)=>{
+    res.send("Resource Not Found")
+})
+
 app.listen(3000, ()=>{
-    console.log("Server is Listening at localhost:3000.......")
+    console.log("Server is Listening at localhost:3000");
 })
