13749af4104504b40871f0ac8b7f58d7aff0a52d
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-24 02:21:25 +0530

    Rendering html file from Server with Resources

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -2,10 +2,33 @@ const http = require('http');
 const {readFileSync}  = require('fs');
 
 const indexhtml = readFileSync('index.html');
+const bootstrapcss = readFileSync('./assets/bootstrap.css');
+const stylecss = readFileSync('./assets/style.css');
+const bannerjpg = readFileSync('./assets/banner.jpg');
 
 const server = http.createServer(function(req, res){
     console.log("Request is Incoming");
     console.log(req.url, req.method);  
+
+    // Setting all the Resources
+    if(req.url=== '/assets/bootstrap.css')
+    { 
+        res.writeHead(200,{'content-type': 'text/css'});
+        res.write(bootstrapcss);
+        res.end();
+    }  
+    if(req.url=== '/assets/style.css')
+    { 
+        res.writeHead(200,{'content-type': 'text/css'});
+        res.write(stylecss);
+        res.end();
+    } 
+    if(req.url=== '/assets/banner.jpg')
+    { 
+        res.writeHead(200,{'content-type': 'image/jpg'});
+        res.write(bannerjpg);
+        res.end();    
+    }     
     
     if(req.url=== '/')
     { 
