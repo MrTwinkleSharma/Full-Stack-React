1bd44794a8a9ba9cc180baa36a39ebb0f7ccecc2
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-26 02:59:38 +0530

    sending json

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -11,6 +11,18 @@ app.use(express.static('./public'));
 //     res.sendFile(path.resolve(__dirname, "./index.html"))
 // })
 
+app.get('/api/myjson', (req,res)=>{
+    res.json({"skills":[
+    "C++", 
+    "GIT", 
+    "Software Development", 
+    "MERN Development"], 
+    "education":[ 
+    {"2018":"Intermediate"},
+    {"2023":"B.Tech (Computer Science & Engineering"}]
+    });
+})
+
 app.get('/about', (req, res)=>{
     res.status(200).send("About Page")
 })
