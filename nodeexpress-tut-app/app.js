8020dc4ba29676a8c809b12389d16eafa56103dc
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-26 01:16:09 +0530

    using index.html as root

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(express.static('./public'));
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.resolve(__dirname, "./index.html"))
-})
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.resolve(__dirname, "./index.html"))
+// })
 
 app.get('/about', (req, res)=>{
     res.status(200).send("About Page")
