9a3d853fc01696798f45a27fa3191dbbb77cc9c3
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-28 00:02:49 +0530

    Using middleware function

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -2,24 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req,res) =>{
-  const method = req.method;
-  const url = req.url;
-  const year = new Date().getFullYear();
-  
-  console.log(method, url, year);
-  res.send("Home Page!");
+const logger = (req, res, next) =>{
+    const method = req.method;
+    const url = req.url;
+    const year = new Date().getFullYear();
+    console.log(method, url, year);
+    // res.send();
+    next();
+}
+
+app.get('/', logger, (req,res) =>{
+ res.send("Home Page!");
 });
 
-app.get('/about', (req,res) =>{
-  const method = req.method;
-  const url = req.url;
-  const year = new Date().getFullYear();
-  
-  console.log(method, url, year);
-  res.send("About Page!");
-});
 
+app.get('/about', logger, (req,res) =>{
+ res.send("Home Page!");    
+});
 
 app.listen(3000, ()=>{
     console.log("Server is Listening at localhost:3000");
