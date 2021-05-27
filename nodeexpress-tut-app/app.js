7267fbb4b8a567c8ce891eb2bc51c9092289c607
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-28 00:04:48 +0530

    Using middleware function

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -1,15 +1,7 @@
 const express = require('express');
 
 const app = express();
-
-const logger = (req, res, next) =>{
-    const method = req.method;
-    const url = req.url;
-    const year = new Date().getFullYear();
-    console.log(method, url, year);
-    // res.send();
-    next();
-}
+const logger = require('./middlewareFn');
 
 app.get('/', logger, (req,res) =>{
  res.send("Home Page!");
@@ -17,7 +9,7 @@ app.get('/', logger, (req,res) =>{
 
 
 app.get('/about', logger, (req,res) =>{
- res.send("Home Page!");    
+ res.send("About Page!");    
 });
 
 app.listen(3000, ()=>{
