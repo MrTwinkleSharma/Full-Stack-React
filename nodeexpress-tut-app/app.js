43f5c0ac79e0ff5684f07f8b9c9a715d0fe02b16
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-28 00:06:00 +0530

    Using middleware function with app.use()

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -3,12 +3,13 @@ const express = require('express');
 const app = express();
 const logger = require('./middlewareFn');
 
-app.get('/', logger, (req,res) =>{
+app.use('/', logger);
+
+app.get('/', (req,res) =>{
  res.send("Home Page!");
 });
 
-
-app.get('/about', logger, (req,res) =>{
+app.get('/about', (req,res) =>{
  res.send("About Page!");    
 });
 
