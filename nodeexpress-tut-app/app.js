40b7072c7613caa7f9648eee8b139c056da6ece2
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-28 22:25:32 +0530

    GET Method

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -1,15 +1,29 @@
 const express = require('express');
-const authorize = require('./middlewareAuthorize');
+const { data } = require('./randomData');
 
 const app = express();
-const logger = require('./middlewareFn');
 
-app.use('/', [logger, authorize]);
 
 app.get('/', (req,res) =>{
  res.send("Home Page!");
 });
 
+app.get('/peoples', (req,res) =>{
+    res.send(JSON.stringify(data));
+});
+   
+
+app.get('/people/id/:placeholder', (req,res) =>{
+    console.log(req.params);
+    let newData = data.find( _data =>{
+        return _data.id==(req.params.placeholder)
+        // console.log(typeof(_data.id));
+        // console.log(typeof(req.params.placeholder));
+    }) 
+    console.log(newData);
+    res.send(JSON.stringify(newData));
+});
+   
 app.get('/about', (req,res) =>{
  res.send("About Page!");    
 });
