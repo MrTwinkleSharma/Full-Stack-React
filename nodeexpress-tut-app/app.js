dabc4c33dcd7a139268b303c7aef33d44f03ca0f
Twinkle Sharma <smtwinkle451@gmail.com>
2021-05-28 23:48:04 +0530

    POST Method

--- nodeexpress-tut-app/app.js
+++ nodeexpress-tut-app/app.js
@@ -3,27 +3,33 @@ const { data } = require('./randomData');
 
 const app = express();
 
-
 app.get('/', (req,res) =>{
  res.send("Home Page!");
 });
 
 app.get('/peoples', (req,res) =>{
-    res.send(JSON.stringify(data));
+    res.send({"success":true,"data":data});
 });
-   
 
 app.get('/people/id/:placeholder', (req,res) =>{
     console.log(req.params);
     let newData = data.find( _data =>{
         return _data.id==(req.params.placeholder)
-        // console.log(typeof(_data.id));
+        // console.log(typeof(_data.id));\
         // console.log(typeof(req.params.placeholder));
     }) 
     console.log(newData);
-    res.send(JSON.stringify(newData));
+    res.send({"success":true,"data":newData});
 });
    
+app.get("/addpeople", (req,res)=>{
+    const str = '<div><h1>Add People</h1><form method="POST"><input type="text" name="people_name"/> <button type="submit">Add</button></form></div>';
+    res.send(str);
+});
+app.post("/addpeople", (req,res)=>{
+    res.send("Resource created successfully!");
+});
+
 app.get('/about', (req,res) =>{
  res.send("About Page!");    
 });
