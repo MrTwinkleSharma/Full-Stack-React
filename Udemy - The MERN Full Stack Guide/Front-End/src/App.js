66b40538d862c632dacdc0a57dabe81be4c3f90f
Twinkle Sharma <smtwinkle451@gmail.com>
2021-06-22 03:11:51 +0530

    Context for Login Added

--- Udemy - The MERN Full Stack Guide/Front-End/src/App.js
+++ Udemy - The MERN Full Stack Guide/Front-End/src/App.js
@@ -1,5 +1,5 @@
 //3rd Party Modules
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {BrowserRouter as Router, Route,Redirect,Switch} from 'react-router-dom';
 
 //Local Modules
@@ -9,41 +9,76 @@ import UserPlaces from './places/Pages/UserPlaces';
 import NewPlace from '../src/places/Pages/NewPlace.js'
 import UpdatePlace from './places/Pages/UpdatePlace';
 import Authenticate from './users/Pages/Authenticate';
+import AuthContext from  './shared/Context/auth-context';
 
 //CSS Files
 import './index.css';
 
 function App (){
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+  let routes;
+
+  if(isLoggedIn){
+    
+    routes = (<Switch>
+      <Route path="/" exact>
+      </Route>
+  
+      <Route path="/:userId/places" exact>
+        <UserPlaces/>
+      </Route>
+  
+      <Route path="/users" exact>  
+        <Users/>
+      </Route>
+      
+      <Route path="/places/new" exact>
+        <NewPlace/>
+      </Route>   
+      
+      <Route path="/places/:placeId" exact>
+        <UpdatePlace/>
+      </Route>    
+  
+      <Redirect to='/' />
+    </Switch>)
+  }
+  else{
+    routes = (<Switch>
+      <Route path="/" exact>
+      </Route>
+
+      <Route path="/auth" exact>
+        <Authenticate/>
+      </Route>
+      
+      <Route path="/:userId/places" exact>
+        <UserPlaces/>
+      </Route>
+
+      <Route path="/users" exact>  
+        <Users/>
+      </Route>
+        
+      <Redirect to='/' />
+    </Switch>)
+
+  }
   return <Router>
+  <AuthContext.Provider value={{isLoggedIn:isLoggedIn, login:login, logout:logout}}>
   <MainNavigation/>
   <main>
-  <Switch>
-    <Route path="/" exact>
-    </Route>
-    <Route path="/auth" exact>
-      <Authenticate/>
-    </Route>
-
-    <Route path="/:userId/places" exact>
-      <UserPlaces/>
-    </Route>
-
-    <Route path="/users" exact>  
-      <Users/>
-    </Route>
-    
-    <Route path="/places/new" exact>
-      <NewPlace/>
-    </Route>   
-    
-    <Route path="/places/:placeId" exact>
-      <UpdatePlace/>
-    </Route>    
-
-    <Redirect to='/' />
-  </Switch>
+  {routes}
   </main>
+  </AuthContext.Provider>
   </Router>
 }
 
--- Udemy - The MERN Full Stack Guide/Front-End/src/shared/Components/Navigation/NavLinks.js
+++ Udemy - The MERN Full Stack Guide/Front-End/src/shared/Components/Navigation/NavLinks.js
@@ -1,23 +1,32 @@
 //3rd Party Modules
-import React from 'react';
+import React, {useContext} from 'react';
 import { NavLink } from 'react-router-dom';
+
+//Local Modules
+import AuthContext from '../../Context/auth-context';
 
 //CSS Files
 import './NavLinks.css';
 
-function Navlinks(props)
-{
+function Navlinks(props){
+
+    const auth = useContext(AuthContext);
+
 
     return <ul className='nav-links'>
     <li>
     <NavLink to='/users' exact> ALL USERS</NavLink>
     </li>
-    <li>
-        <NavLink to='/:userId/places' >MY PLACES</NavLink>
-    </li> 
-    <li>
-        <NavLink to='/places/new' >ADD PLACE</NavLink>
-    </li> 
+    { auth.isLoggedIn &&
+        <li>
+            <NavLink to='/:userId/places' >MY PLACES</NavLink>
+        </li> 
+    }
+    { auth.isLoggedIn &&
+        <li>
+            <NavLink to='/places/new' >ADD PLACE</NavLink>
+        </li> 
+    }
     <li>
         <NavLink to='/auth' >AUTHENTICATE</NavLink>
     </li> 
