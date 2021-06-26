7ebf84339816437a70ac5d17de52bc67ad0161da
Twinkle Sharma <smtwinkle451@gmail.com>
2021-06-27 02:22:09 +0530

    Adding Configured Multer to Backend

--- Udemy - The MERN Full Stack Guide/Back-End/routes/users-routes.js
+++ Udemy - The MERN Full Stack Guide/Back-End/routes/users-routes.js
@@ -1,16 +1,19 @@
 //3rd Party Modules
 const { Router } = require('express');
 const { check } = require('express-validator');
-
+const { default: fileUpload } = require('../middleware/fileUpload');
+ 
 const router = Router();
 const {
         signUp, 
         logIn, 
         getUsers } = require('../controllers/users-controllers');
- 
+
 router.get('/', getUsers);
 
-router.post('/signup', [
+router.post('/signup',
+        fileUpload.single('image'),
+        [
         check('email').normalizeEmail().isEmail(),
         check('name').isLength({min:3}),
         check('password').isLength({min:6})
