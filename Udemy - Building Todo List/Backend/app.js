3dadba005d67d9e961d6b259c400115e9a80b59e
Twinkle Sharma <smtwinkle451@gmail.com>
2021-07-03 03:29:31 +0530

    Connecting to DataBase

--- Udemy - Building Todo List/Backend/app.js
+++ Udemy - Building Todo List/Backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const todoRoutes = require('./routes/todoRoutes.js')
 const app = express();
 const PORT = process.env.PORT || '5000';
@@ -11,6 +12,13 @@ app.get('/', (req, res)=>{
     res.send('HOME PAGE');
 });
 
-app.listen(PORT, ()=>{
-console.log("Server is Listening on Port " + PORT);
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mongodbcluster.mcnv5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+                { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true})
+.then(()=>{
+    app.listen(PORT, ()=>{
+        console.log("Server is Listening on Port " + PORT);
+    })
+})
+.catch((err)=>{
+    console.log("A error has been occured while connecting to database!");    
 })
