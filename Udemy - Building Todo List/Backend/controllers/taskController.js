c3f7a67405344285f024a05041b7f3f84bd6ed40
Twinkle Sharma <smtwinkle451@gmail.com>
2021-07-03 17:57:40 +0530

    CRUD IN MONGOOSE All 4 http verbs in one commit

--- Udemy - Building Todo List/Backend/controllers/taskController.js
+++ Udemy - Building Todo List/Backend/controllers/taskController.js
@@ -1,58 +1,49 @@
 const dummyList = require('../dummyList.js');
+const TaskModel = require('../models/TaskModel.js');
 
-const getTasks = (req, res) =>{
-    
-    res.status(200).json({success:true, data:dummyList})
+const getTasks = async (req, res) =>{
+    const list = await TaskModel.find();
+    res.status(200).json({success:true, data:list})
 }
 
-const postTask = (req, res) =>{
+const postTask = async (req, res) =>{
     const {title, description} = req.body;
-    const task = {
+    const task = TaskModel({
         title, 
         description,
         isCompleted:false
-    };
+    });
 
-    dummyList.push(task);
-    res.status(201).json({success:true, data:dummyList});
-
+    await task.save();
+    res.status(201).json({success:true, data:task});
 }
 
-const patchTask = (req, res) =>{
+const patchTask = async (req, res) =>{
     const {taskId} = req.params;
-    console.log(taskId);
-
     const {title, description} = req.body;
-    let found = false;
-
-    const updatedList = dummyList.map(listItem =>{
-            if(listItem.title==taskId)
-            {
-                listItem.title=title
-                listItem.description=description
-                found=true;
-            }
-            return listItem;
-        });
-
-    if(!found)
+    
+    const existingTask = await TaskModel.findById(taskId);
+    if(!existingTask)
     {
         res.status(404).json({success:false, message:"No task found with given id!"});
     }
-    else
-    res.status(200).json({success:true, data:updatedList}); 
+    existingTask.title = title;
+    existingTask.description = description;
+    
+    
+    res.status(200).json({success:true, data:existingTask}); 
 }
 
-const deleteTask = (req, res) =>{
+const deleteTask = async (req, res) =>{
     const {taskId} = req.params;
 
-    const updatedList = dummyList.filter(listItem=>listItem.title!==taskId);
-
-    if(updatedList.length===dummyList.length)
+    const existingTask = await TaskModel.findById(taskId);
+    if(!existingTask)
     {
         res.status(404).json({success:false, message:"No task found with given id!"});
     }
-    else
+    await existingTask.remove();
+    
     res.status(200).json({success:true, message:"Task deleted Successfully!"}); 
 }
 
